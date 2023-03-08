75ca8c7dbac3e701e878abb97c914c0cea9703a9
marianatheml <marianatheml@gmail.com>
2023-03-07 20:18:11 -0800

    add prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,18 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Mariana Theml</title>
-        <meta name="description" content="Mariana Theml | My personal portfolio to showcase my work and skills as a software developer." />
+        <meta
+          name="description"
+          content="Mariana Theml | My personal portfolio to showcase my work and skills as a software developer."
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-      </main>
+      <main></main>
     </>
-  )
+  );
 }
