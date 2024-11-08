acb9c3492ed43410cecef57dda8a44a9ef69fe5f
marianatheml <marianatheml@gmail.com>
2024-11-07 17:03:41 -0800

    fixing typo error

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+// src/pages/_document.tsx
 import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
@@ -5,7 +6,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Faculty+Glyphic&display=swap" rel="stylesheet" />
       </Head>
       <body>
