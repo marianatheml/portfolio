8cbb8a81b9e3fc5aa2c429c19a0119f5e8a4d9bc
marianatheml <marianatheml@gmail.com>
2024-11-07 23:29:34 -0800

    fix typo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { useState } from 'react';
-import styles from '../styles/components/navbar.module.css';
+import styles from '../styles/components/Navbar.module.css';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
